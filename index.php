c9e2d4a1646c64034f27bdfdc5970721b69501cc
Joan <joanvila2006@gmail.com>
2025-12-15 20:45:32 +0100

    Feature Done: Tasks

--- index.php
+++ index.php
@@ -2,6 +2,22 @@
 // Sistema de Gestió de Tasques
 // Versió 1.0
 
-echo "=== GESTOR DE TASQUES ===\n";
-echo "Benvingut al sistema!\n";
+function mostrarTasques($tasques) {
+    echo "=== LLISTA DE TASQUES ===</br>";
+    foreach($tasques as $id => $descripcio) {
+        echo "[$id] $descripcio</br>";
+    }
+}
+
+// Crea un array amb almenys 3 tasques
+$tasques = [
+    1 => "Treure al gos",
+    2 => "Treure la basura",
+    3 => "Llimpiar l'habitacio",
+    4 => "Fer un backup",
+];
+
+echo "=== GESTOR DE TASQUES ===</br>";
+echo "Benvingut al sistema!</br>";
+mostrarTasques($tasques)
 ?>
